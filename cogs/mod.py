fbd559fb7d2ad888359cbfcbee06c3d74a23871d
Luffy D <84648682+BinaryBills@users.noreply.github.com>
2023-02-12 04:39:56 -0500

    Clear, Kick, and Ban update!

--- cogs/mod.py
+++ cogs/mod.py
@@ -22,31 +22,53 @@ class mod(commands.Cog):
     @app_commands.checks.bot_has_permissions(manage_messages = True)
     @app_commands.command(name = "clear", description = "Clears a specified amount of messages")
     async def clear(self, interaction: discord.Interaction, amount : int):
-     await interaction.response.defer(ephemeral = True)  
-     await interaction.channel.purge(limit=amount)   
-     await interaction.followup.send(f"{interaction.user.mention}, the previous **{amount} messages** have been removed!")
-          
+     await interaction.response.defer(ephemeral = True) 
+     if amount < 1: 
+        await interaction.followup.send(f"{interaction.user.mention}, you must specify a positive integer")
+     elif amount > 100:
+        await interaction.followup.send(f"{interaction.user.mention}, you cannot delete more than 100 messages at a time")
+     else:
+        messages = []
+        async for message in interaction.channel.history(limit=amount):
+            messages.append(message)
+        if len(messages) == 0:
+            await interaction.followup.send(f"{interaction.user.mention}, there are no messages to delete.")
+            return
+        deleted = 0
+        for x in range(0, amount):
+            if x >= len(messages):
+                break
+            try:
+                await messages[x].delete()
+                deleted += 1
+                if (x % 13 == 0):  
+                    await asyncio.sleep(2) 
+            except discord.NotFound:
+                continue
+        await interaction.followup.send(f"{interaction.user.mention}, the previous **{deleted} messages** have been removed!")
+
+
     @app_commands.checks.has_permissions(kick_members=True)
     @app_commands.checks.bot_has_permissions(kick_members = True)
     @app_commands.command(name = "kick", description = "Kicks a user")
     async def kick(self, interaction: discord.Interaction, member:discord.Member, reason:str):
-          temp = member
-          if await is_owner(interaction,member) == False:
-           await interaction.guild.kick(member)
-           await interaction.response.send_message(f"{temp.mention} has been kicked! Reason: {reason}!")
-          else:
-              await interaction.response.send_message("User specified is a privileged user!")
+     temp = member
+     if await is_owner(interaction, member) == False and (interaction.user.id != member.id) and (not member.guild_permissions.kick_members):
+        await interaction.guild.kick(member)
+        await interaction.response.send_message(f"{temp.mention} has been kicked! Reason: {reason}!")
+     else:
+        await interaction.response.send_message("User specified is a privileged user or has the kick members permission!")
          
     @app_commands.checks.has_permissions(ban_members=True)
     @app_commands.checks.bot_has_permissions(ban_members = True)
     @app_commands.command(name = "ban", description = "Bans a user")
     async def ban(self, interaction: discord.Interaction, member:discord.Member, reason:str):
           temp = member
-          if await is_owner(interaction,member) == False:
+          if await is_owner(interaction, member) == False and (interaction.user.id != member.id) and (not member.guild_permissions.ban_members):
            await interaction.guild.ban(member)
            await interaction.response.send_message(f"{temp.mention} has been banned! Reason: {reason}!")
           else:
-              await interaction.response.send_message("User specified is a privileged user!")
+              await interaction.response.send_message("User specified is a privileged user or has the ban members permission!")
     
     @app_commands.checks.has_permissions(manage_messages=True)
     @app_commands.checks.bot_has_permissions(manage_messages=True)
