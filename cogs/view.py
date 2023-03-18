f42b528b9b702338f96a19c0df4b367030950080
Luffy D <84648682+BinaryBills@users.noreply.github.com>
2023-03-18 15:23:49 -0400

    Correct Timeout

--- cogs/view.py
+++ cogs/view.py
@@ -4,8 +4,7 @@ from discord import app_commands
 from discord import ui
 from config import sqlServer
 from config import settings
-
-#Note: There is a bug that when it timeouts, the message interaction failed is displayed
+import asyncio
 
 
 class Paginator(ui.View):
@@ -17,14 +16,19 @@ class Paginator(ui.View):
 
     async def send_initial_message(self, interaction):
         try:
-            self.message = await interaction.response.send_message(
-                embed=self.create_embed(self.get_current_page_data()), view=self, ephemeral=False
+            await interaction.response.defer(ephemeral=True)
+            self.message = await interaction.channel.send(
+                embed=self.create_embed(self.get_current_page_data()), view=self
             )
+            self.last_interaction = interaction
+            asyncio.create_task(self.delete_message_after_timeout())
             return self.message
         except Exception as e:
             print(e)
 
+
     def create_embed(self, data):
+        """Sets the embed layout for the table and each singular element listed in the table"""
         embed = discord.Embed(
             title=f"Resources Page {self.current_page + 1} / {len(self.data) // self.items_per_page + 1}",
             description="Here are the test resources:",
@@ -44,14 +48,17 @@ class Paginator(ui.View):
         end = start + self.items_per_page
         return self.data[start:end]
 
-    @ui.button(
-        label="Previous", style=discord.ButtonStyle.primary, custom_id="previous_button", row=0
-    )
+    @ui.button(label="Previous", style=discord.ButtonStyle.primary, custom_id="previous_button", row=0)
     async def previous_page(self, interaction: discord.Interaction, button: discord.ui.Button):
+        """This function allows the user to move one page backwards in the paginated table."""
+        
+        #If the current page is the first page, we go to the last page.
+        #If the current page is not the first page, we move to the previous page.
         if self.current_page == 0:
             self.current_page = len(self.data) // self.items_per_page
         else:
             self.current_page -= 1
+            
         try:
             await interaction.response.edit_message(embed=self.create_embed(self.get_current_page_data()), view=self)
         except discord.errors.NotFound:
@@ -61,18 +68,41 @@ class Paginator(ui.View):
 
     @ui.button(label="Next", style=discord.ButtonStyle.primary, custom_id="next_button", row=0)
     async def next_page(self, interaction: discord.Interaction, button: discord.ui.Button):
+        """This function allows the user to move one page forward in the paginated table."""
+        
+        #If the current page is the last page, we go to the first page.
+        #If the current page is not the last page, we move forward one page. 
         if self.current_page == len(self.data) // self.items_per_page:
             self.current_page = 0
         else:
             self.current_page += 1
+              
         try:
             await interaction.response.edit_message(embed=self.create_embed(self.get_current_page_data()), view=self)
         except discord.errors.NotFound:
             pass
         except Exception as e:
             print(e)
+            
+    async def delete_message_after_timeout(self):
+        await asyncio.sleep(self.timeout)
+        await self.delete_message()
 
-
+    async def delete_message(self):
+        try:
+             await self.message.delete()
+             await self.last_interaction.followup.send("The resource page has timed out.")
+        except discord.errors.NotFound:
+            pass
+        except Exception as e:
+            print(e)
+           
+            
+            
+    async def on_timeout(self):
+        pass
+        
+        
 class searchEngine(commands.Cog):
     def __init__(self, client):
         self.client = client
@@ -80,6 +110,7 @@ class searchEngine(commands.Cog):
     @app_commands.command(name="view", description="View academic resources!")
     async def view(self, interaction: discord.Interaction):
         try:
+            #Grab correct information from the table
             cursor = settings.conn.cursor()
             cursor.execute("SELECT * FROM academic_resources")
             data = cursor.fetchall()
